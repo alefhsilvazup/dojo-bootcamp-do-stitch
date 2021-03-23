3b31f8924c3910f9e9e8688d2b30baf1e4599552
Afonso Mateus <afonsoprad10@gmail.com>
2021-03-23 16:25:41 -0400

    gravando e realizando a resposa

--- src/main/java/br/com/zupacademy/dojoot3/controller/TurmaController.java
+++ src/main/java/br/com/zupacademy/dojoot3/controller/TurmaController.java
@@ -4,20 +4,28 @@ import br.com.zupacademy.dojoot3.form.TurmaForm;
 import br.com.zupacademy.dojoot3.entity.Turma;
 import br.com.zupacademy.dojoot3.form.TurmaResponseForm;
 import br.com.zupacademy.dojoot3.repository.TurmaRepository;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/turmas")
 public class TurmaController {
 
-    @Autowired private TurmaRepository turmaRepository;
+    @Autowired
+    private TurmaRepository turmaRepository;
 
     @PostMapping
     public ResponseEntity<TurmaForm> cadastrarTurma(@RequestBody TurmaForm form) {
         Turma turma = form.converter();
-        turma=turmaRepository.save(turma);
+        turma = turmaRepository.save(turma);
+        TurmaResponseForm response = new TurmaResponseForm(turma);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(response.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(response);
 
     }
 }
