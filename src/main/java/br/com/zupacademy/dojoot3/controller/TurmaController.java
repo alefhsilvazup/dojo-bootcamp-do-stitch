9800297d74c484a60ce0e26962257a6bff705bf5
Isadora Chagas <isadoragchagas@outlook.com>
2021-03-23 17:06:21 -0300

    Implementando a Controller

--- src/main/java/br/com/zupacademy/dojoot3/controller/TurmaController.java
+++ src/main/java/br/com/zupacademy/dojoot3/controller/TurmaController.java
@@ -1,13 +1,20 @@
 package br.com.zupacademy.dojoot3.controller;
 
 import br.com.zupacademy.dojoot3.TurmaForm;
+import br.com.zupacademy.dojoot3.entity.Turma;
+import br.com.zupacademy.dojoot3.repository.TurmaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/turmas")
 public class TurmaController {
+
+    @Autowired private TurmaRepository turmaRepository;
+
     @PostMapping
-    public String cadastrarTurma(@RequestBody TurmaForm form) {
-        
+    public ResponseEntity<TurmaForm> cadastrarTurma(@RequestBody TurmaForm form) {
+        Turma turma = form.converter()
     }
 }
