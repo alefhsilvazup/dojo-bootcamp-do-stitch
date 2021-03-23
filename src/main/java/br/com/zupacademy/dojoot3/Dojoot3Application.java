b101229d175b70789dc56e0facc46700cbc2ce57
alefhsousa <alefh.sousa@gmail.com>
2021-03-23 16:22:59 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupacademy/dojoot3/Dojoot3Application.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.dojoot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dojoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dojoot3Application.class, args);
+	}
+
+}
